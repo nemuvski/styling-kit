48202f5034dfb32d8fcb5aae8f36ed6fceb79a26
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-01-27 14:28:31 +0900

    ビルド時にstylelintが走るようにする

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const stylelint = require('gulp-stylelint');
 const postcss = require('gulp-postcss');
 
 gulp.task('build', () => {
-  return gulp.src('./scss/**/*.s[ca]ss')
-    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+  return gulp
+    .src('./scss/**/*.s[ca]ss')
     .pipe(
-      postcss([
-        require('autoprefixer'),
-        require('postcss-nested')
-      ])
+      stylelint({
+        failAfterError: false,
+        reporters: [{ formatter: 'string', console: true }],
+      })
     )
-    .pipe(gulp.dest("./css"))
-})
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(postcss([require('autoprefixer'), require('postcss-nested')]))
+    .pipe(gulp.dest('./css'));
+});
 
 gulp.task('build:watch', () => {
-  gulp.watch('./scss/**/*.s[ca]ss', gulp.series('build'))
-})
+  gulp.watch('./scss/**/*.s[ca]ss', gulp.series('build'));
+});
