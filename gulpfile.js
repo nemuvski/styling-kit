b4d7916f32593b04819b4789149976dc1594f104
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-03-09 16:08:32 +0900

    SVGファイルを扱うケース

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,31 @@ const clip = require('gulp-clip-empty-files');
 const sass = require('gulp-sass')(require('sass'));
 const stylelint = require('gulp-stylelint');
 const postcss = require('gulp-postcss');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+
+function optSvg(done) {
+  gulp
+    .src('./assets/*.orig.svg', { allowEmpty: true })
+    .pipe(
+      imagemin([
+        imagemin.svgo({
+          plugins: [{ removeViewBox: false }],
+        }),
+      ])
+    )
+    .pipe(rename((filepath) => {
+      // e.g. path={ dirname: '.', basename: 'sample.orig', extname: '.svg' }
+      return {
+        dirname: filepath.dirname,
+        // 末尾の.origを除く
+        basename: filepath.basename.replace(/\.orig$/, ''),
+        extname: filepath.extname,
+      }
+    }))
+    .pipe(gulp.dest('./assets'));
+  done();
+}
 
 function buildSass(done) {
   gulp
@@ -25,5 +50,6 @@ function watchSass() {
   gulp.watch('./scss/**/*.s[ca]ss', gulp.series('buildSass'));
 }
 
+exports.optSvg = optSvg;
 exports.buildSass = buildSass;
 exports.watchSass = watchSass;
